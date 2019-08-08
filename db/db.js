5f47c42220e959fe71ec00154b46f14a7d8fedcc
liamgibbs <liamsamuelgibbs@hotmail.com>
2019-08-08 16:26:07 +0100

    Init commit
    Provide counter parties API call

---
+++ db/db.js
@@ -0,0 +1,18 @@
+const counterParties = ["Goldman Sachs", "JP Morgan", "Morgan Stanley", "Citi Bank"];
+const users = ["johndoe", "exponentialcherub"];
+const tradeAssociations = [
+    {"username": "johndoe",
+        "party": "JP Morgan"},
+    {"username": "johndoe",
+        "party": "Morgan Stanley"},
+    {"username": "exponentialcherub",
+        "party": "Goldman Sachs"},
+    {"username": "exponentialcherub",
+        "party": "Citi Bank"},
+    {"username": "exponentialcherub",
+        "party": "JP Morgan"}];
+
+
+exports.counterParties = counterParties;
+exports.users = users;
+exports.tradeAssociations = tradeAssociations;
