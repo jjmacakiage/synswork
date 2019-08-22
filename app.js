7addb364a95ea56f68aa3b98cd533822152b815b
liamgibbs <liamsamuelgibbs@hotmail.com>
2019-08-22 14:38:55 +0100

    Change api to call mock blockchain instead of ethereum smart contracts

--- app.js
+++ app.js
@@ -1,66 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Web3 = require('web3');
+const axios = require('axios');
 
 const app = express();
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-const abi = require("./abi").abi;
-const utils = require("./utils");
-
-const address = "0x4923A8e4bEd25D7a1a0aB3D25e2Ac1A6211ec033";
-
-const sender = "0xBE14623B7E18293a8aAfD2c60d5Ad3Bee6c089e7";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const address = 'http://localhost:4500';
 
 /**
  * This creates a trade agreement between the two parties given, allowing them to allege trades to the other.
  */
 app.post('/api/tradeagreements', (req, res) => {
-    const contract = new web3.eth.Contract(abi, address);
-
-    contract.methods.addTradeAgreement(req.body.id1, req.body.id2).send({from: sender, gas: 5000000}, function(err, result){
-        if(!err){
+    axios.post(address + '/tradeagreements', {id1: req.body.id1, id2: req.body.id2})
+        .then(response => {
             return res.status(200).send({
                 success: true,
                 message: "New trade agreement succesfully added.",
             });
-        }
-        else{
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error adding trade agreement.'
+                success: false,
+                message: "Error adding trade agreement. "
             });
-        }
-    });
+        });
 });
 
 /**
  * This endpoint gets all the counterparties a given counterparty (id) can trade with.
  */
 app.get('/api/parties/:id/counterparties', (req, res) => {
-    const contract = new web3.eth.Contract(abi, address);
-
-    contract.methods.getCounterParties(req.params.id).call(function(err, result){
-        if(!err && result != null){
-            const counterParties = utils.deserializeStringArray(result);
-
+    const id = parseInt(req.params.id, 10);
+    axios.get(address + '/parties/' + id + '/counterparties')
+        .then(response => {
             return res.status(200).send({
-                success: 'true',
-                message: 'Counterparties successfully received.',
-                counterparties: counterParties
+                success: true,
+                counterparties: response.data.counterparties,
             });
-        }
-        else{
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error retrieving counterparties.'
+                success: false,
+                message: "Error getting counterparties"
             });
-        }
-    });
+        });
 });
 
 /**
@@ -75,23 +62,20 @@ app.post('/api/parties', (req, res) => {
         return;
     }
 
-    const contract = new web3.eth.Contract(abi, address);
-
-    contract.methods.addCounterParty(req.body.party).send({from: sender, gas: 999999}, function(err, result){
-        if(!err){
+    axios.post(address + '/parties', {party: req.body.party})
+        .then(response => {
             return res.status(200).send({
                 success: true,
                 message: "New counterparty succesfully added.",
             });
-        }
-        else{
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error adding counterparty.'
+                success: false,
+                message: "Error adding counterparty."
             });
-        }
-    });
+        });
 });
 
 /**
@@ -100,58 +84,44 @@ app.post('/api/parties', (req, res) => {
 app.get('/api/parties/:partyid/trades/:tradeid' , (req, res) => {
     const tradeid = parseInt(req.params.tradeid, 10);
     const partyid = parseInt(req.params.partyid, 10);
-    const contract = new web3.eth.Contract(abi, address);
 
-    contract.methods.getTradeInfo(tradeid, partyid).call(function(err, result){
-        if(!err && parseInt(result[0]) !== 0) {
+    axios.get(address + '/parties/' + partyid + '/trades/' + tradeid)
+        .then(response => {
             return res.status(200).send({
-                success: 'true',
-                message: 'Trade successfully received.',
-                id: result[0],
-                party1: result[1],
-                party2: result[2],
-                status: result[3]
+                success: true,
+                tradeInfo: response.data.tradeInfo,
             });
-        }
-        else {
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error getting trade info, trade may not exist.'
+                success: false,
+                message: "Error getting trade info."
             });
-        }
-    });
+        });
 });
 
 /**
  * This gets all trades for a particular counterparty across all trade agreements.
- * TODO: Currently, the result is given as a set of arrays as opposed to an array of structs.
- * TODO: This should change in the future as this will not be ideal once all the trade info has been modelled.
  */
 app.get('/api/parties/:id/trades', (req, res) => {
     const id = parseInt(req.params.id, 10);
-    const contract = new web3.eth.Contract(abi, address);
 
-    contract.methods.getAllTradeInfo(id).call(function(err, result){
-        if(!err && parseInt(result[0]) !== 0) {
-            console.log(result);
+    axios.get(address + '/parties/' + id + '/trades/')
+        .then(response => {
+            console.log(response);
             return res.status(200).send({
-                success: 'true',
-                message: 'Trade successfully received.',
-                id: result[0],
-                party1: result[1],
-                party2: result[2],
-                status: result[3]
+                success: true,
+                trades: response.data.trades,
             });
-        }
-        else {
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error getting trade info, trade may not exist.'
+                success: false,
+                message: "Error getting trade info."
             });
-        }
-    });
+        });
 });
 
 /**
@@ -166,22 +136,21 @@ app.post('/api/parties/:id/trades', (req, res) => {
         return;
     }
 
-    const contract = new web3.eth.Contract(abi, address);
-    contract.methods.addTrade(partyid, req.body.counpartyid).send({from: sender, gas: 979800}, function(err, result){
-        if(!err){
+    const id = parseInt(req.params.id, 10);
+    axios.post(address + '/parties/' + id + '/trades', {counterpartyid: req.body.counterpartyid})
+        .then(response => {
             return res.status(200).send({
                 success: true,
-                message: "New trade succesfully created."
+                message: "New trade succesfully added.",
             });
-        }
-        else{
-            console.log(err);
+        })
+        .catch(error => {
+            console.log(error);
             return res.status(400).send({
-                success: 'false',
-                message: 'Error creating trade.'
+                success: false,
+                message: "Error adding trade."
             });
-        }
-    });
+        });
 });
 
 app.use(function(req, res) {
